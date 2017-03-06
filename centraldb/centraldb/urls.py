756eda6f926805936f0750d0df6c7949163fa395
Melvyn Petrocchi <melvyn.petrocchi@epita.fr>
2017-03-06 20:37:04 +0100

    [CONFIG] Custom the view url

--- centraldb/centraldb/urls.py
+++ centraldb/centraldb/urls.py
@@ -1,7 +1,7 @@
 '''centraldb/urls.py'''
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('centraldb_site/urls.py')),
+    url(r'^', include('centraldb_site.urls')),
 ]
