4ea29b4de8bded293f1e53bb5e26aacce0017174
Melvyn Petrocchi <melvyn.petrocchi@epita.fr>
2017-03-07 14:05:31 +0100

    [DESING] add the bootstrap design

--- centraldb/centraldb_site/urls.py
+++ centraldb/centraldb_site/urls.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^test$', views.test, name='test'),
+  #  url(r'^addition/(?P<nombre1>\d+)/(?P<nombre2>\d+)/$', views.addition)
 ]
--- centraldb/centraldb_site/views.py
+++ centraldb/centraldb_site/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("hello");
+  return render(request, 'centraldb/home.html');
+def test(request):
+  return render(request, 'centraldb/test.html');
